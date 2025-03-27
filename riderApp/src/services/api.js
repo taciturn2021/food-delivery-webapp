a2dce5f191bc96745577532e2edff28bdc263cdf
taciturn2021 <142534049+taciturn2021@users.noreply.github.com>
2025-03-27 21:50:07 +0500

    fix: Remove fallback URL for API_BASE_URL in configuration

--- riderApp/src/services/api.js
+++ riderApp/src/services/api.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { API_BASE_URL } from '@env';
 
 // Configure API base URL from environment variable
-const BASE_URL = API_BASE_URL || 'http://192.168.31.215:5001/api'; // Fallback if env variable is not loaded
+const BASE_URL = API_BASE_URL ; // Fallback if env variable is not loaded
 
 // Create axios instance
 const api = axios.create({
